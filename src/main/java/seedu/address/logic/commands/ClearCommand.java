8a7406b4bb0d14d10bf6e6ad21b3815ccc1f7e56
jeraldkiew <jeraldkiew007@gmail.com>
2023-03-26 06:34:58 +0800

    refactor clearCommand

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.patient.Patient;
 
 /**
  * Clears the address book.
@@ -17,6 +18,7 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        Patient.resetCount();
         model.setAddressBook(new AddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
